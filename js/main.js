574112a20074df595b93ff7fb102a62dea855e72
shota0810 <syota.u1104@gmail.com>
2023-06-10 22:36:47 +0900

    add message text

--- js/main.js
+++ js/main.js
@@ -5,22 +5,31 @@
     const clear = document.querySelector('#clear');
     const message = document.querySelector('#message');
 
+// ローカルストレージにデータがあるか確認
     if(localStorage.getItem('memo') === null){
         text.value = '';
     }else{
         text.value = localStorage.getItem('memo');
     }
 
+// 入力したテキストをローカルストレージに保存
     save.addEventListener('click', () => {
-        message.classList.add('appear');
+        message.textContent = 'saved'
+        message.classList.add('appear','saved');
         setTimeout(() => {
-            message.classList.remove('appear');
+            message.classList.remove('appear','saved');
         }, 1000);
         localStorage.setItem('memo',text.value);
     });
 
+// ローカルストレージからデータを削除
     clear.addEventListener('click',()=>{
         if(confirm('本当に削除しますか?') === true){
+            message.textContent = 'cleared'
+            message.classList.add('appear','cleared');
+            setTimeout(() => {
+                message.classList.remove('appear','cleared');
+            }, 1000);
             text.value = '';
             localStorage.removeItem('memo');
         }
